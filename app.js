5746473a57eaf7dae215a6fb5ab10dbefb5b0535
marcovittoriozacca <zaccamarco01@gmail.com>
2024-05-22 14:57:04 +0200

    fix: Season attribute + added objects.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const objects = require('./objects');
+
 //class Movie
 class Movie {
     constructor(title, year, genre, rating, type){
@@ -15,19 +17,12 @@ class Movie {
 
 class TvSerie extends Movie {
 
-    constructor(title, year, genre, rating, type, season){
+    constructor(title, year, genre, rating, type, seasons){
         super(title, year, genre, rating, type);
-        this.season = season;
+        this.seasons = seasons;
     }
 
     toString(){
-        return `${this.title} è una serie tv del genere ${this.genre}. La prima stagione è stata rilasciata nel ${this.year} ed in totale sono state prodotte ${this.season} stagioni. Ha avuto un voto di ${this.rating}.`;
+        return `${this.title} è una serie tv del genere ${this.genre}. La prima stagione è stata rilasciata nel ${this.year} ed in totale sono state prodotte ${this.seasons} stagioni. Ha avuto un voto di ${this.rating}.`;
     }
 }
-// Breaking Bad è una serie tv di genere Drama. La prima stagione è stata rilasciato nel 2008 ed in totale sono state prodotte 5 stagioni. Ha un voto di 9.5
-
-const movie = new Movie('Jaws', 1975, 'Drama', 8, 'Movie' );
-const tvSerie = new TvSerie('Breaking Bad', 2008, 'Drana', 9.5, 'TvSerie', '5');
-
-console.log(movie.toString());
-console.log(tvSerie.toString());
---
+++ objects.js
@@ -0,0 +1,169 @@
+const objects = [
+    {
+        "title": "Jaws",
+        "year": 1975,
+        "genre": "Drama",
+        "rating": 8,
+        "type": "movie",
+    },
+    {
+        "title": "The Godfather",
+        "year": 1972,
+        "genre": "Crime",
+        "rating": 9,
+        "type": "movie"
+    },
+    {
+        "title": "Star Wars: Episode IV - A New Hope",
+        "year": 1977,
+        "genre": "Sci-Fi",
+        "rating": 8,
+        "type": "movie"
+    },
+    {
+        "title": "Pulp Fiction",
+        "year": 1994,
+        "genre": "Crime",
+        "rating": 8,
+        "type": "movie"
+    },
+    {
+        "title": "The Shawshank Redemption",
+        "year": 1994,
+        "genre": "Drama",
+        "rating": 9,
+        "type": "movie"
+    },
+    {
+        "title": "The Dark Knight",
+        "year": 2008,
+        "genre": "Action",
+        "rating": 9,
+        "type": "movie"
+    },
+    {
+        "title": "Schindler's List",
+        "year": 1993,
+        "genre": "History",
+        "rating": 9,
+        "type": "movie"
+    },
+    {
+        "title": "Forrest Gump",
+        "year": 1994,
+        "genre": "Drama",
+        "rating": 8,
+        "type": "movie"
+    },
+    {
+        "title": "Inception",
+        "year": 2010,
+        "genre": "Sci-Fi",
+        "rating": 8,
+        "type": "movie"
+    },
+    {
+        "title": "Fight Club",
+        "year": 1999,
+        "genre": "Drama",
+        "rating": 8,
+        "type": "movie"
+    },
+    {
+        "title": "The Matrix",
+        "year": 1999,
+        "genre": "Sci-Fi",
+        "rating": 8,
+        "type": "movie"
+    },
+    {
+        "title": "Breaking Bad",
+        "year": 2008,
+        "genre": "Drama",
+        "rating": 9.5,
+        "type": "tv",
+        "seasons": 5,
+    },
+    {
+        "title": "Game of Thrones",
+        "year": 2011,
+        "genre": "Fantasy",
+        "rating": 9.3,
+        "type": "tv",
+        "seasons": 8
+    },
+    {
+        "title": "Stranger Things",
+        "year": 2016,
+        "genre": "Sci-Fi",
+        "rating": 8.7,
+        "type": "tv",
+        "seasons": 4
+    },
+    {
+        "title": "The Sopranos",
+        "year": 1999,
+        "genre": "Crime",
+        "rating": 9.2,
+        "type": "tv",
+        "seasons": 6
+    },
+    {
+        "title": "The Office",
+        "year": 2005,
+        "genre": "Comedy",
+        "rating": 8.9,
+        "type": "tv",
+        "seasons": 9
+    },
+    {
+        "title": "Friends",
+        "year": 1994,
+        "genre": "Comedy",
+        "rating": 8.8,
+        "type": "tv",
+        "seasons": 10
+    },
+    {
+        "title": "Sherlock",
+        "year": 2010,
+        "genre": "Crime",
+        "rating": 9.1,
+        "type": "tv",
+        "seasons": 4
+    },
+    {
+        "title": "The Mandalorian",
+        "year": 2019,
+        "genre": "Sci-Fi",
+        "rating": 8.7,
+        "type": "tv",
+        "seasons": 3
+    },
+    {
+        "title": "The Crown",
+        "year": 2016,
+        "genre": "Drama",
+        "rating": 8.6,
+        "type": "tv",
+        "seasons": 6
+    },
+    {
+        "title": "Black Mirror",
+        "year": 2011,
+        "genre": "Sci-Fi",
+        "rating": 8.8,
+        "type": "tv",
+        "seasons": 5
+    },
+    {
+        "title": "Westworld",
+        "year": 2016,
+        "genre": "Sci-Fi",
+        "rating": 8.6,
+        "type": "tv",
+        "seasons": 4
+    }   
+]
+
+module.exports = objects;
